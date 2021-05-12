52453c7ecb9629379f17439196b8dd6104de8d95
Ning <1097984277@qq.com>
2021-05-12 14:36:09 +0800

    1.封装axios

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+//引入封装好的axios
+import api from './http/index'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+//引入封装好的axios
+Vue.use(api)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/mock/mock.js
+++ src/mock/mock.js
@@ -8,4 +8,11 @@ Mock.mock('http://localhost:8080/menu',{
   'id':'@Increment',
   'name':'menu',
   'order|10-20':12
-})
+});
+//模拟返回登录
+Mock.mock('http://localhost:8080/login',{
+    data:{
+      'token':'3132312321312'
+    }
+});
+
