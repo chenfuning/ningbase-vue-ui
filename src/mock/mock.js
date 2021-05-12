b352033f2c9e4278063d1223f1fb13652c476cf3
Ning <1097984277@qq.com>
2021-05-12 11:08:24 +0800

    1.整合mock

---
+++ src/mock/mock.js
@@ -0,0 +1,11 @@
+import  Mock from 'mockjs';//ES6 语法引入mock模块
+Mock.mock('http://localhost:8080/user',{
+  'name':'@name',//随机生成姓名
+  'email':'@email',
+  'age|1-10':5
+});
+Mock.mock('http://localhost:8080/menu',{
+  'id':'@Increment',
+  'name':'menu',
+  'order|10-20':12
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,22 +2,39 @@
   <div class="page">
     <h2>Home Page</h2>
     <el-button type="primary" @click="testAxios()">测试axios</el-button>
+    <el-button type="primary" @click="testuser()">测试mock user</el-button>
+    <el-button type="primary" @click="testmenu()">测试mock menu</el-button>
   </div>
 </template>
 
 <script>
   import axios from 'axios'
+  import mock from '@/mock/mock.js';
   export default{
     name:'Home',
     methods:{
       testAxios() {
         axios.get('http://localhost:8080')
           .then(function (res){
-            alert(res.data);
+            alert(JSON.stringify(res.data));
           }).catch(function (res){
           alert(res.data);
         })
-      }
+      },
+      testuser(){
+        axios.get('http://localhost:8080/user')
+          .then(function (res){
+            alert(JSON.stringify(res.data));
+          }).catch(function (res){
+          alert(res.data);
+        })
+      },
+      testmenu(){axios.get('http://localhost:8080/menu')
+        .then(function (res){
+          alert(JSON.stringify(res.data));
+        }).catch(function (res){
+          alert(res.data);
+        })}
     }
   }
 </script>
