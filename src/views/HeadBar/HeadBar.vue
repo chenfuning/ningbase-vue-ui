a169623063d07eb16e590c01fdd595d3cffb656e
Ning <1097984277@qq.com>
2021-05-20 11:10:39 +0800

    1.主页面分模块

--- src/views/HeadBar/HeadBar.vue
+++ src/views/HeadBar/HeadBar.vue
@@ -1,5 +1,30 @@
 <template>
-
+  <div class="container">
+    <!-- 导航菜单隐藏显示切换 -->
+    <span class="collapse-switcher" @click.prevent="collapse">
+      <i class="el-icon-menu"></i>
+    </span>
+    <!-- 导航菜单 -->
+    <span class="nav-bar">
+      <el-menu class="el-menu-demo" text-color="#fff"
+               active-text-color="#ffd04b" mode="horizontal" @select="selectNavBar()">
+        <el-menu-item index="1" @click="$router.push('/')">首页</el-menu-item>
+        <el-menu-item index="2">消息中心</el-menu-item>
+        <el-menu-item index="3">订单管理</el-menu-item>
+      </el-menu>
+    </span>
+    <span class="tool-bar">
+      <!-- 用户信息 -->
+      <el-dropdown class="user-info-dropdown" trigger="hover">
+        <span class="el-dropdown-link"><img :src="this.userAvatar" /> {{username}}</span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>我的消息</el-dropdown-item>
+          <el-dropdown-item>设置</el-dropdown-item>
+          <el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </span>
+  </div>
 </template>
 
 <script>
@@ -7,17 +32,86 @@ import mock from "@/mock/index.js";
 export default {
   data() {
     return {
+      username:"Ning",
+      userAvatar: "",
     };
   },
   methods: {
-
+    //退出登录
+    logout: function () {
+      var _this = this;
+      this.$confirm("确认退出吗?", "提示", {
+        type: "warning"
+      })
+        .then(() => {
+          sessionStorage.removeItem("user");
+          this.$router.push("/login");
+        })
+        .catch(() => {
+        });
+    }
   },
   mounted() {
-
+    var user = sessionStorage.getItem("user");
+    if (user) {
+      this.username = user;
+      this.userAvatar = require("@/assets/user.png");
+    }
   }
 };
 </script>
 
 <style scoped lang="scss">
+.container {
+  position: absolute;
+  left: 200px;
+  right: 0px;
+  height: 60px;
+  line-height: 60px;
+  .collapse-switcher {
+    width: 40px;
+    float: left;
+    cursor: pointer;
+    border-color: rgba(111, 123, 131, 0.8);
+    border-left-width: 1px;
+    border-left-style: solid;
+    border-right-width: 1px;
+    border-right-style: solid;
+    color: white;
+    background: #504e6180;
+  }
+  .nav-bar {
+    margin-left: auto;
+    float: left;
+    .el-menu {
+      background: #504e6180;
+    }
+  }
+  .tool-bar {
+    float: right;
+    .theme-picker {
+      padding-right: 10px;
+    }
+    .lang-selector {
+      padding-right: 10px;
+      font-size: 15px;
+      color: #fff;
+      cursor: pointer;
+    }
+    .user-info-dropdown {
+      font-size: 20px;
+      padding-right: 20px;
+      color: #fff;
+      cursor: pointer;
+      img {
+        width: 40px;
+        height: 40px;
+        border-radius: 10px;
+        margin: 10px 0px 10px 10px;
+        float: right;
+      }
+    }
+  }
+}
 
 </style>
--- src/views/NavBar/NavBar.vue
+++ src/views/NavBar/NavBar.vue
@@ -1,7 +1,28 @@
 <template>
+  <div class="menu-bar-container">
+    <!-- logo -->
+    <div class="logo" :class="isCollapse?'menu-bar-collapse-width':'menu-bar-width'">
+      <img :src="this.logo" /> <div>{{isCollapse?'':sysName}}</div>
+    </div>
+    <!-- 导航菜单 -->
+    <el-menu default-active="1-1" :class="isCollapse?'menu-bar-collapse-width':'menu-bar-width'">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">系统管理</span>
+        </template>
+        <el-menu-item index="1-1" @click="$router.push('user')">用户管理</el-menu-item>
+        <el-menu-item index="1-2" @click="$router.push('menu')">菜单管理</el-menu-item>
 
-
-
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">系统介绍</span>
+        </template>
+      </el-submenu>
+    </el-menu>
+  </div>
 </template>
 
 
@@ -9,17 +30,53 @@
 export default {
   data() {
     return {
+      isCollapse:false,
+      sysName: "NingBase",
+      logo: "",
     };
   },
   methods: {
 
   },
   mounted() {
-
+    this.sysName = "NingBase";
+    this.logo = require("@/assets/logo.png");
   }
 };
 </script>
 
 <style scoped lang="scss">
-
+.menu-bar-container {
+  .el-menu {
+    position:absolute;
+    top: 60px;
+    bottom: 0px;
+    text-align: left;
+  }
+  .logo {
+    position:absolute;
+    top: 0px;
+    height: 60px;
+    line-height: 60px;
+    background: #4b5f6e;
+    img {
+      width: 40px;
+      height: 40px;
+      border-radius: 0px;
+      margin: 10px 10px 10px 10px;
+      float: left;
+    }
+    div {
+      font-size: 22px;
+      color: white;
+      text-align: left;
+    }
+  }
+  .menu-bar-width {
+    width: 200px;
+  }
+  .menu-bar-collapse-width {
+    width: 65px;
+  }
+}
 </style>
